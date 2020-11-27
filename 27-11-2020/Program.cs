e0dcfafdc50f1ba80f4af1166305148daa86e9e4
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-11-27 20:14:54 +0000

    updating program.cs

--- 27-11-2020/Program.cs
+++ 27-11-2020/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _27_11_2020
 {
@@ -6,7 +7,21 @@ namespace _27_11_2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Song s1 = new Song("ED SHEERAN", "BEAUTIFUL PEOPLE", 3.15, Genre.Pop);
+            Song s2 = new Song("REGARD", "RIDE IT", 3.37, Genre.Dance);
+            Song s3 = new Song("TONES & I", "DANCE MONKEY", 4.20, Genre.Dance);
+            Song s4 = new Song("POST MALONE", "CIRCLES", 3.25, Genre.Pop);
+            Song s5 = new Song("ED SHEERAN", "SOUTH OF THE BORDER", 4.26, Genre.Other);
+            List<Song> playlist = new List<Song>();
+            playlist.Add(s1);
+            playlist.Add(s2);
+            playlist.Add(s3);
+            playlist.Add(s4);
+            playlist.Add(s5);
+            foreach (Song song in playlist)
+            {
+                Console.WriteLine(song);
+            }
         }
     }
 }
