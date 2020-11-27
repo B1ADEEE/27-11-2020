7d6cd32d876d08f6f92b1dca2d37819316b9d5c2
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-11-27 19:46:08 +0000

    1st lot of work done in the class Song

---
+++ 27-11-2020/Song.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _27_11_2020
+{
+    class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre SongGenre { get; set; }
+        public Song(string title, string artist,double duration,Genre songGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            SongGenre = songGenre;
+        }
+        public Song(string title,string artist)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = 0;
+            SongGenre = Genre.Other;
+        }
+        public Song():this("unknown", "unknown"){ }
+        public override string ToString()
+        {
+            return string.Format($"{Artist}{Title}{Duration}{SongGenre}");
+        }
+    }
+    public enum Genre {  Rock, Pop, Dance, Other}
+}
